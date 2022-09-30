b0b7982e8f442d44a7024015119d3efcf675a2c6
amir-hu <hussain.amir.978@gmail.com>
2022-09-29 23:10:39 -0400

    wanna check something

--- Pages/Scripts/webScrap.js
+++ Pages/Scripts/webScrap.js
@@ -1,11 +1,32 @@
-function scrap() {
-    const request = require('request')
-    request('https://www.reddit.com/r/programming.json', function (
-        error,
-        response,
-        body
-    ) {
-        console.error('error:', error)
-        console.log('body:', body)
-    })
-}
+//const bing = require('bing-image-search-api-scraper');
+
+    //foo is now loaded.
+//define(['require', 'bing-image-search-api-scraper'], function (require) {
+const bing = require('bing-image-search-api-scraper ');
+const axios = require('axios');
+const cheerio = require('cheerio');
+//});
+
+const image = document.createElement('img');
+
+//let bing = bing-image-search-api-scraper()
+var search = bing.search('god of war box cover').then((res) => {
+    var image = res[0].photo;
+    console.log("hrlooooooooooooo");
+    //console.log(decodeURIComponent(image.split("&mediaurl=")[1].split("&")[0]));
+    newthing = decodeURIComponent(image.split("&mediaurl=")[1].split("&")[0]);
+    console.log(image);
+    image.setAttribute(
+        'src',
+        newthing,
+    );
+});
+
+
+image.setAttribute('alt', 'nature');
+
+
+image.setAttribute('width', 400);
+
+const box = document.getElementById('info');
+    box.appendChild(image);
