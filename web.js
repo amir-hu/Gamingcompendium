05079e03ca353aeb9e3087a726a1d6d8d2afed12
Amir Hussain <55963887+amir-hu@users.noreply.github.com>
2022-10-16 00:43:10 -0400

    Update web.js

--- web.js
+++ web.js
@@ -1,11 +1,11 @@
 var path = require('path'); 		    // some convenient dir/path functions
 var express = require('express');	    // use the express module
-const cors = require('cors')
+const cors = require('cors');
 var app = express();			        // this is our express.js instance
 const PORT = process.env.PORT || 5000;  // Port should be 5000 by default
-const cors = require("cors");
 
-//app.use(cors());
+
+app.use(cors());
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
